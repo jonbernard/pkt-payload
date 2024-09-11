7430abce477b6040a59fe7753cf43c02856844d2
Jon Bernard <jon@jonbernard.net>
2024-09-11 15:07:40 -0400

    fix spacing

--- src/app/(app)/components/richText/content.tsx
+++ src/app/(app)/components/richText/content.tsx
@@ -1,13 +1,14 @@
 import { SerializedLexicalNode } from './types';
 import Content from '.';
 import { Stack } from '@mui/material';
+import classNames from 'classnames';
 
 const ContentBlock = (content: SerializedLexicalNode) => {
   return (
     <Stack direction="row" spacing={4} alignItems={content.fields.align}>
       {content.fields.columns?.map((column: any) => {
         return (
-          <div key={column.id} className={column.size}>
+          <div key={column.id} className={classNames(column.size, 'space-y-6')}>
             {column.richText?.root?.children &&
               column.richText?.root?.children.map(
                 (content: any, index: number) => content && <Content key={index} data={content} excludeContainer />,
