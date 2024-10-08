34d30660591d1c7f835398a918dbcfe60bcf79c5
Jon Bernard <jon@jonbernard.net>
2024-10-08 18:53:05 -0400

    link list

--- src/blocks/Content/editor.ts
+++ src/blocks/Content/editor.ts
@@ -3,9 +3,9 @@ import {
   FixedToolbarFeature,
   HeadingFeature,
   InlineToolbarFeature,
-  lexicalEditor,
 } from '@payloadcms/richtext-lexical';
 
+import { LinkList } from '../LinkList';
 import { MediaBlock } from '../MediaBlock';
 import { PaymentLinkBlocks } from '../PaymentLink';
 import { Video } from '../Video';
@@ -13,7 +13,7 @@ import { Video } from '../Video';
 export const editorFeatures = (includeMedia = true) => [
   HeadingFeature({ enabledHeadingSizes: ['h2', 'h3', 'h4'] }),
   BlocksFeature({
-    blocks: [PaymentLinkBlocks, ...(includeMedia ? [MediaBlock] : []), Video],
+    blocks: [LinkList, PaymentLinkBlocks, ...(includeMedia ? [MediaBlock] : []), Video],
   }),
   FixedToolbarFeature(),
   InlineToolbarFeature(),
