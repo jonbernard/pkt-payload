afcd2faa30961f6d25695b43024e8ef636789f11
Jon Bernard <jon@jonbernard.net>
2024-10-08 21:45:17 -0400

    update

--- src/app/(app)/components/richText/linkList.tsx
+++ src/app/(app)/components/richText/linkList.tsx
@@ -7,23 +7,25 @@ import { SerializedLexicalNode } from './types';
 
 const LinkList = (content: SerializedLexicalNode) => {
   return (
-    <Box className="inline-block">
-      {content.fields?.links?.map((link: { url: string; label: string }) => (
-        <Stack
-          component={Link}
-          key={link.url}
-          href={link.url}
-          direction="row"
-          alignItems="center"
-          justifyContent="center"
-          spacing={6}
-          className="disableLinkStyles border-b border-solid border-gray-300 dark:border-gray-700"
-        >
-          <h2>{link.label}</h2>
-          <ArrowForwardIosIcon />
-        </Stack>
-      ))}
-    </Box>
+    <Stack direction="row" alignItems="center" justifyContent="center" className="w-full">
+      <Box className="inline-block">
+        {content.fields?.links?.map((link: { url: string; label: string }) => (
+          <Stack
+            component={Link}
+            key={link.url}
+            href={link.url}
+            direction="row"
+            alignItems="center"
+            justifyContent="center"
+            spacing={6}
+            className="disableLinkStyles border-b border-solid border-gray-300 dark:border-gray-700"
+          >
+            <h2>{link.label}</h2>
+            <ArrowForwardIosIcon />
+          </Stack>
+        ))}
+      </Box>
+    </Stack>
   );
 };
 
